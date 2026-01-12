1c28cc3e3bcc604a34cc2ec1fbc8b7941a3e0d30
unknown <christianagot11@gmail.com>
2026-01-12 22:31:03 +0800

    completed v1 layout of login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
   return (
-    <section className="w-full h-screen flex justify-center items-center">
-      <form className="flex flex-col items-center gap-4">
-        <div className="flex flex-col gap-2">
-          <label for="login-email">Email</label>
-          <input type="email" id="login-email"/>
+    <section className="w-full h-screen px-4 flex justify-center items-center">
+      <form className="bg-slate-100 w-100 border-2 rounded-xl px-12 py-6 flex flex-col items-center gap-4">
+        <h2 className="text-xl">
+          LOGIN
+        </h2>
+        <div className="w-full flex flex-col gap-0.5">
+          <label className="text-sm" for="login-email">Email</label>
+          <input type="email" id="login-email" className="bg-white text-sm border px-2 py-1 rounded-md" required/>
         </div>
-        <div>
-          <label for="login-password">Password</label>
-          <input type="password" id="login-password"/>
+        <div className="w-full flex flex-col gap-0.5">
+          <label className="text-sm" for="login-password">Password</label>
+          <span className="w-full">
+            <input type="password" id="login-password" className="w-full bg-white text-sm border px-2 py-1 rounded-md" required/>
+          </span>
+        </div>
+        <div className="w-4/5 flex flex-col mt-4">
+          <input type="submit" id="login-submit" value="Login" className="w-full text-white bg-black mb-2 border py-1 rounded-md cursor-pointer hover:scale-105 transition-[scale]" />
+          <span className="w-full text-center text-sm">
+            Don't have an account? {" "}
+            <Link to="/sign-up" className="whitespace-nowrap text-blue-900 hover:text-blue-600 hover:underline">
+              Sign up
+            </Link>
+          </span>
         </div>
       </form>
     </section>
