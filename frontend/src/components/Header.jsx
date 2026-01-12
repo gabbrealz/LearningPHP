9130a8cb1ae9485afcb8d3e33eaffeae06d1b645
unknown <christianagot11@gmail.com>
2026-01-13 02:20:28 +0800

    changed header bg-color and navlink button colors

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,16 +2,22 @@ import { Link } from "react-router-dom"
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full py-4 bg-black px-4 sm:px-12 md:px-20 lg:px-28 xl:px-36">
+    <header className="fixed top-0 left-0 w-full py-4 bg-gray-900 px-4 sm:px-12 md:px-20 lg:px-28 xl:px-36">
       <nav className="w-full flex">
         <Link to="/" className="text-xl text-white px-2 py-1 flex items-center rounded-lg hover:text-slate-400 transition-[color]">
           GABBREALZ
         </Link>
-        <div className="ml-auto w-1/2 flex justify-end items-center gap-4">
-          <Link to="/login" className="text-sm text-white px-4 py-1 border rounded-lg hover:border-green-700 hover:bg-green-700 transition-colors">
+        <div className="ml-auto w-1/2 flex justify-end items-center gap-2 lg:gap-4">
+          <Link to="/login" className="
+            text-sm text-white px-2 py-1 border rounded-lg border-green-700 bg-green-700 transition-colors
+            lg:px-4 lg:border-white lg:bg-transparent lg:hover:border-green-700 lg:hover:bg-green-700
+          ">
             Login
           </Link>
-          <Link to="/sign-up" className="text-sm text-white px-4 py-1 border rounded-lg hover:border-indigo-500 hover:bg-indigo-500 transition-colors">
+          <Link to="/sign-up" className="
+            text-sm text-white px-2 py-1 border rounded-lg border-indigo-600 bg-indigo-600 transition-colors
+            lg:px-4 lg:border-white lg:bg-transparent lg:hover:border-indigo-600 lg:hover:bg-indigo-600
+          ">
             Sign Up
           </Link>
         </div>
