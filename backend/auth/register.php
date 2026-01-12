9445c8436b99d8b94f35b0db38e208e794a4efe4
unknown <christianagot11@gmail.com>
2026-01-12 20:50:45 +0800

    added headers for cross-origin http request

--- backend/auth/register.php
+++ backend/auth/register.php
@@ -1,3 +1,12 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+session_start();
+?>
+
 <html>
 <body>
 
