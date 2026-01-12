0de9f01300059c58d3d06c296bad0e1dcc85dbf1
unknown <christianagot11@gmail.com>
2026-01-12 20:47:36 +0800

    updated App.jsx and added react routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header.jsx';
+import Landing from './pages/Landing.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import Footer from './components/Footer.jsx';
 import './assets/styles.css';
 
-function App() {
+export default function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Header/>
+
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+
+      <Footer/>
+    </BrowserRouter>
   )
 }
-
-export default App
