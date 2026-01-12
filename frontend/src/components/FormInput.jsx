624f3057bbf3a4826fd5cd6d9dec1aa6c04d99d6
unknown <christianagot11@gmail.com>
2026-01-13 03:01:18 +0800

    added show/hide password functionality. increased input heights via padding

--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -1,20 +1,31 @@
+import { useState } from "react";
+import ShowPassIcon from "../assets/interface-icons/eye.svg?react";
+import HidePassIcon from "../assets/interface-icons/eye-crossed.svg?react";
+
 export function FormInput({ formName, inputName, label, type, className }) {
   return (
     <div className={`w-full flex flex-col gap-0.5 ${className}`}>
       <label className="text-sm" for={`${formName}-${inputName}`}>{label}</label>
       <input type={type} id={`${formName}-${inputName}`} name={inputName}
-             className="bg-white text-sm border border-gray-500 px-2 py-1 rounded-md" required/>
+             className="bg-white text-sm border border-gray-500 p-2 rounded-md" required/>
     </div>
   );
 }
 
 export function PasswordInput({ formName, inputName, label, className }) {
+  const [show, setShow] = useState(false)
+
   return (
     <div className={`w-full flex flex-col gap-0.5 ${className}`}>
       <label className="text-sm" for={`${formName}-${inputName}`}>{label}</label>
-      <span>
-        <input type="password" id={`${formName}-${inputName}`} name={inputName}
-               className="w-full bg-white text-sm border border-gray-500 px-2 py-1 rounded-md" required/>
+      <span className="relative">
+        <input type={show ? "text" : "password"} id={`${formName}-${inputName}`} name={inputName}
+               className="w-full bg-white text-sm border border-gray-500 p-2 rounded-md" required/>
+        {
+          show ?
+            <HidePassIcon className="cursor-pointer absolute size-4.5 top-1/2 right-1 -translate-1/2" onClick={() => setShow(false)} /> : 
+            <ShowPassIcon className="cursor-pointer absolute size-4.5 top-1/2 right-1 -translate-1/2" onClick={() => setShow(true)} />
+        }
       </span>
     </div>
   );
