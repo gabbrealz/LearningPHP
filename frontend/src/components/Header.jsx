78369d9d0826000b8590ee36fcec218a10b7304c
unknown <christianagot11@gmail.com>
2026-01-12 22:31:53 +0800

    added router links to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,13 +2,18 @@ import { Link } from "react-router-dom"
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full px-36 py-4 bg-slate-300">
-      <nav className="w-full">
-        <Link className="text-lg">
-          gabbrealz
+    <header className="fixed top-0 left-0 w-full py-4 bg-black px-4 sm:px-12 md:px-20 lg:px-28 xl:px-36">
+      <nav className="w-full flex">
+        <Link to="/" className="text-xl text-white px-2 py-1 flex items-center rounded-lg hover:text-slate-400 transition-[color]">
+          GABBREALZ
         </Link>
-        <div className="ml-auto w-1/2">
-          
+        <div className="ml-auto w-1/2 flex justify-end items-center gap-4">
+          <Link to="/login" className="text-sm text-white px-4 py-1 border rounded-lg hover:border-green-700 hover:bg-green-700 transition-colors">
+            Login
+          </Link>
+          <Link to="/sign-up" className="text-sm text-white px-4 py-1 border rounded-lg hover:border-indigo-500 hover:bg-indigo-500 transition-colors">
+            Sign Up
+          </Link>
         </div>
       </nav>
     </header>
