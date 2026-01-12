966ad9955c967224f0fb383e08795eb6fca8b355
unknown <christianagot11@gmail.com>
2026-01-13 02:19:03 +0800

    used form input components. replaced email input in login with username/email. changed styling a bit on both forms

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,17 @@
 import { Link } from "react-router-dom";
+import { FormInput, PasswordInput } from "../components/FormInput";
 
 export default function Login() {
   return (
     <section className="w-full h-screen px-4 flex justify-center items-center">
-      <form className="bg-gray-100 w-100 border-2 rounded-xl px-12 py-6 flex flex-col items-center gap-4">
+      <form className="bg-gray-100 w-100 border-2 border-gray-400 rounded-xl px-12 py-6 flex flex-col items-center gap-4">
         <h2 className="text-xl">
           LOGIN
         </h2>
-        <div className="w-full flex flex-col gap-0.5">
-          <label className="text-sm" for="login-email">Email</label>
-          <input type="email" id="login-email" className="bg-white text-sm border px-2 py-1 rounded-md" required/>
-        </div>
-        <div className="w-full flex flex-col gap-0.5">
-          <label className="text-sm" for="login-password">Password</label>
-          <span className="w-full">
-            <input type="password" id="login-password" className="w-full bg-white text-sm border px-2 py-1 rounded-md" required/>
-          </span>
-        </div>
+        <FormInput formName="login" inputName="username-or-email" label="Username / Email" type="text" />
+        <PasswordInput formName="login" inputName="password" label="Password" />
         <div className="w-4/5 flex flex-col mt-4">
-          <input type="submit" id="login-submit" value="Login" className="w-full text-white bg-black mb-2 border py-1 rounded-md cursor-pointer hover:bg-gray-800 transition-colors" />
+          <input type="submit" id="login-submit" value="Login" className="w-full text-white bg-green-700 mb-2 border py-1.5 rounded-md cursor-pointer hover:bg-green-600 transition-colors" />
           <span className="w-full text-center text-sm">
             Don't have an account? {" "}
             <Link to="/sign-up" className="whitespace-nowrap text-blue-900 hover:text-blue-600 hover:underline">
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,31 +1,18 @@
+import { FormInput, PasswordInput } from "../components/FormInput.jsx";
+
 export default function SignUp() {
   return (
     <section className="w-full h-screen px-4 flex justify-center items-center">
-      <form className="bg-gray-100 w-100 border-2 rounded-xl px-12 py-6 flex flex-col items-center gap-4">
+      <form className="bg-gray-100 w-100 border-2 border-gray-400 rounded-xl px-12 py-6 flex flex-col items-center gap-4">
         <h2 className="text-xl">
           SIGN UP
         </h2>
-        <div className="w-full flex flex-col gap-0.5">
-            <label className="text-sm" for="signup-name">Username</label>
-            <input type="text" id="signup-name" className="bg-white text-sm border px-2 py-1 rounded-md" required/>
-        </div>
-        <div className="w-full flex flex-col gap-0.5">
-          <label className="text-sm" for="signup-email">Email</label>
-          <input type="email" id="signup-email" className="bg-white text-sm border px-2 py-1 rounded-md" required/>
-        </div>
-        <div className="w-full flex flex-col gap-0.5">
-          <label className="text-sm" for="signup-password">Password</label>
-          <span>
-            <input type="password" id="signup-password" className="w-full bg-white text-sm border px-2 py-1 rounded-md" required/>
-          </span>
-        </div>
-        <div className="w-full flex flex-col gap-0.5">
-          <label className="text-sm" for="signup-confirm-password">Confirm Password</label>
-          <span className="w-full">
-            <input type="password" id="signup-confirm-password" className="w-full bg-white text-sm border px-2 py-1 rounded-md" required/>
-          </span>
-        </div>
-        <input type="submit" id="signup-submit" value="Sign Up" className="w-4/5 text-white bg-black mt-4 border py-1 rounded-md cursor-pointer hover:bg-gray-800 transition-colors" />
+        <FormInput formName="signup" inputName="username" label="Username" type="text" />
+        <FormInput formName="signup" inputName="email" label="Email" type="email" />
+        <PasswordInput formName="signup" inputName="password" label="Password" />
+        <PasswordInput formName="signup" inputName="confirm-password" label="Confirm Password" />
+        <input type="submit" id="signup-submit" value="Sign Up"
+               className="w-4/5 text-white bg-indigo-600 mt-4 py-1.5 rounded-md cursor-pointer hover:bg-indigo-500 transition-colors" />
       </form>
     </section>
   );
