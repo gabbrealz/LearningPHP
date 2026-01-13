dba24e11c006c84575b495276d80208a98872f42
unknown <christianagot11@gmail.com>
2026-01-14 01:12:32 +0800

    changed spacings and styling when showing warning messages (form input)

--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -4,11 +4,11 @@ import HidePassIcon from "../assets/interface-icons/eye-crossed.svg?react";
 
 export function FormInput({ formName, inputName, label, type, value, setValue, warningMessage, className, showWarning = false }) {
   return (
-    <div className={`w-full flex flex-col gap-0.5 ${className}`}>
-      <label className="text-sm" htmlFor={`${formName}-${inputName}`}>{label}</label>
+    <div className={`w-full flex flex-col ${className}`}>
+      <label className="text-sm mb-0.5" htmlFor={`${formName}-${inputName}`}>{label}</label>
       <input type={type} id={`${formName}-${inputName}`} name={inputName} value={value} onChange={(e) => setValue(e.target.value)}
-             className="bg-white text-sm border border-gray-500 p-2 rounded-md" required/>
-      {
+             className={`bg-white text-sm border border-gray-500 p-2 ${showWarning ? "rounded-t-md" : "rounded-md"}`} required/>
+      { 
         warningMessage === undefined ? null :
         <div className={`bg-red-700 text-xs text-white text-center px-8 py-1 rounded-b-md ${showWarning ? "" : "h-0 scale-y-0"} transition-[height,scale]`}>
           {warningMessage}
@@ -22,11 +22,11 @@ export function PasswordInput({ formName, inputName, label, value, setValue, war
   const [show, setShow] = useState(false)
 
   return (
-    <div className={`w-full flex flex-col gap-0.5 ${className}`}>
-      <label className="text-sm" htmlFor={`${formName}-${inputName}`}>{label}</label>
+    <div className={`w-full flex flex-col ${className}`}>
+      <label className="text-sm mb-0.5" htmlFor={`${formName}-${inputName}`}>{label}</label>
       <span className="relative">
         <input type={show ? "text" : "password"} id={`${formName}-${inputName}`} name={inputName} value={value} onChange={(e) => setValue(e.target.value)}
-               className="w-full bg-white text-sm border border-gray-500 p-2 rounded-md" required/>
+               className={`w-full bg-white text-sm border border-gray-500 p-2 ${showWarning ? "rounded-t-md" : "rounded-md"}`} required/>
         {
           show ?
             <HidePassIcon className="cursor-pointer absolute size-4.5 top-1/2 right-1 -translate-1/2" onClick={() => setShow(false)} /> : 
