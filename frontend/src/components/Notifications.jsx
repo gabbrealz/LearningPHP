196389aa036abc764d1732a5eb6131743b4bb632
unknown <christianagot11@gmail.com>
2026-01-14 00:15:46 +0800

    updated notifications styling. added close button icon svg

--- frontend/src/components/Notifications.jsx
+++ frontend/src/components/Notifications.jsx
@@ -1,21 +1,26 @@
+import CloseIcon from "../assets/interface-icons/cross.svg?react";
+
 export default function Notifications({ notifStack, setNotifStack }) {
   return (
     <>
       {
         notifStack.map((notif, index) => {
-          const scale = 1 - index * 0.05;
-          
           return (
             <div key={notif.id} className={`
-              w-1/2 fixed top-8 left-1/2 -translate-x-1/2
-              p-4 text-sm text-white transition-transform
+              w-1/2 fixed top-4 left-1/2 -translate-x-1/2 rounded-full
+              p-2.5 text-sm text-white transition
               ${notif.bgcolor}
-            `} style={{ transform: `translateY(${index*4}px) scale(${scale})` }}>
-              
-              {notif.message}
-              <span className="absolute top-1/2 right-4 -translate-y-1/2 p-2 cursor-pointer"
-                    onClick={() => setNotifStack(notifStack.filter((entry) => entry !== notif.id))}>
-                🗙
+            `} style={{
+              transform: `translateY(${index*6}px) scaleX(${1 - index*0.05})`,
+              zIndex: notifStack.length-index,
+              filter: `brightness(${1 - index*0.2})`
+            }}>
+              <div className="text-center select-none">
+                {notif.message}
+              </div>
+              <span className={`group absolute top-1/2 right-4 -translate-y-1/2 p-1.5 border-2 border-white rounded-full hover:bg-white transition-colors ${index === 0 ? "cursor-pointer" : ""}`}
+                    onClick={index !== 0 ? () => {} : () => setNotifStack(notifStack.filter((entry) => entry.id !== notif.id))}>
+                <CloseIcon className="size-2.5 fill-white group-hover:fill-black transition-colors"/>
               </span>
 
             </div>
