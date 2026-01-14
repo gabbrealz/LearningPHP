f87c1b11885ec001a5427de2e8bb420744111541
unknown <christianagot11@gmail.com>
2026-01-14 13:07:48 +0800

    removed unneeded allowed methods in php response headers

--- backend/auth/user.php
+++ backend/auth/user.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
