faa463a46fd4919e0742bca08719662db8076b31
unknown <christianagot11@gmail.com>
2026-01-14 20:15:23 +0800

    changed landing bg image

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,39 +2,37 @@ import { Link } from "react-router-dom";
 
 export default function Landing({ authenticatedUser }) {
   return (
-    <section className="w-full h-screen bg-[url(../assets/background/landing-bg.svg)] bg-cover bg-center">
-      <div className="size-full bg-black/20 flex justify-center items-center">
-        <h1 className="flex flex-col items-center">
-          {
-            authenticatedUser ?
-              <>
-                <span className="text-6xl mb-6 whitespace-nowrap">
-                  Good day! Welcome back,
-                </span>
-                <span className="text-8xl underline underline-offset-10 text-indigo-900">
-                  {authenticatedUser}
-                </span>
-              </>
-              :
-              <>
-                <span className="text-8xl mb-8 whitespace-nowrap">
-                  Hey there 😊
-                </span>
-                <span className="text-4xl text-center leading-normal">
-                  You're not logged in right now.<br/>
-                  Kindly {" "}
-                  <Link to="/login" className="whitespace-nowrap text-blue-900 hover:text-blue-600 hover:underline">
-                    Login
-                  </Link>
-                  {" "} or {" "}
-                  <Link to="/sign-up" className="whitespace-nowrap text-blue-900 hover:text-blue-600 hover:underline">
-                    Sign up
-                  </Link>!
-                </span>
-              </>
-          }
-        </h1>
-      </div>
+    <section className="w-full h-screen flex justify-center items-center bg-[url(../assets/background/landing-bg.svg)] bg-cover bg-center">
+      <h1 className="flex flex-col items-center">
+        {
+          authenticatedUser ?
+            <>
+              <span className="text-6xl mb-6 whitespace-nowrap">
+                Good day! Welcome back,
+              </span>
+              <span className="text-8xl underline underline-offset-10 text-indigo-900">
+                {authenticatedUser}
+              </span>
+            </>
+            :
+            <>
+              <span className="text-8xl mb-8 whitespace-nowrap">
+                Hey there 😊
+              </span>
+              <span className="text-4xl text-center leading-normal">
+                You're not logged in right now.<br/>
+                Kindly {" "}
+                <Link to="/login" className="whitespace-nowrap text-blue-900 hover:text-blue-600 hover:underline">
+                  Login
+                </Link>
+                {" "} or {" "}
+                <Link to="/sign-up" className="whitespace-nowrap text-blue-900 hover:text-blue-600 hover:underline">
+                  Sign up
+                </Link>!
+              </span>
+            </>
+        }
+      </h1>
     </section>
   );
 }
