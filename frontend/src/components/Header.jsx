4723e63a5e9b1eb52c95d3b767a11f6e16ffe822
unknown <christianagot11@gmail.com>
2026-01-16 01:11:10 +0800

    changed header brand nav link to 'ExpenseTracker' (still not final)

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -54,7 +54,7 @@ export default function Header({ addToNotifs, setModalData, authenticatedUser, s
     <header className="z-100 fixed top-0 left-0 w-full py-4 bg-black px-4 sm:px-12 md:px-20 lg:px-28 xl:px-36">
       <nav className="w-full flex">
         <Link to="/" className="text-xl text-white px-2 py-1 flex items-center rounded-lg hover:text-slate-400 transition-[color]">
-          GABBREALZ
+          ExpenseTracker
         </Link>
         <div className="ml-auto w-1/2 flex justify-end items-center gap-2 lg:gap-4">
           { authenticatedUser ?
