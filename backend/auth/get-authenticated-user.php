56d2128d4f11f4c66e1bea01317fa0b922ca851c
unknown <christianagot11@gmail.com>
2026-01-15 18:11:22 +0800

    added a check for remember me cookie on get-authenticated-user.php

--- backend/auth/get-authenticated-user.php
+++ backend/auth/get-authenticated-user.php
@@ -15,7 +15,40 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
 
 http_response_code(200);
 
-if (isset($_SESSION["user_id"]))
+if (isset($_SESSION["user_id"])) {
     echo json_encode(["authenticated" => true, "username" => $_SESSION["user_name"]]);
-else
-    echo json_encode(["authenticated" => false]);
+    exit;
+}
+
+if (count($_COOKIE) > 0) {
+    if (isset($_COOKIE["LEARNINGPHP_REMEMBERME_COOKIE"])) {
+        $cookie_key = $_COOKIE["LEARNINGPHP_REMEMBERME_COOKIE"];
+
+        $rememberme_data_file = "../data/remember-me.json";
+        try { $rememberme_data = json_decode(file_get_contents($rememberme_data_file), true); }
+        catch (Exception $e) { $rememberme_data = []; }
+
+        if (isset($rememberme_data[$cookie_key])) {
+            $user_rememberme = $rememberme_data[$cookie_key];
+
+            $user_data_file = "../data/users.json";
+            try { $user_data = json_decode(file_get_contents($user_data_file), true); }
+            catch (Exception $e) { $user_data = ["users" => []]; }
+            $users = $user_data["users"];
+
+            if (isset($users[$user_rememberme["user_id"]])) {
+                $_SESSION["user_id"] = $user_rememberme["user_id"];
+                $_SESSION["user_name"] = $users[$user_rememberme["user_id"]]["user_name"];
+
+                echo json_encode(["authenticated" => true, "username" => $_SESSION["user_name"]]);
+                exit;
+            }
+        }
+        else {
+            setcookie("LEARNINGPHP_REMEMBER_ME", $cookie_key, time() - 3600, "/");
+            // Put code here that erases the cookie in json   
+        }
+    }
+}
+
+echo json_encode(["authenticated" => false]);
