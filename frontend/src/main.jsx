6b11dde832569096011ce9e92fdd25cfacbb1d31
unknown <christianagot11@gmail.com>
2026-01-19 02:19:29 +0800

    set up chart.js in project (not used yet)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import {
+  Chart as ChartJS, CategoryScale, LinearScale,
+  BarElement, LineElement, PointElement,
+  ArcElement, Tooltip, Legend
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale, LinearScale, BarElement,
+  LineElement, PointElement, ArcElement,
+  Tooltip, Legend
+);
+
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
