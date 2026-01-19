7da5d500be43bb07d2b54db21066c13168273f1f
unknown <christianagot11@gmail.com>
2026-01-19 15:14:04 +0800

    added bootstrap.php and db-init.sql io initialize db and provide PDO if they don't exist yet

---
+++ backend/config/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__ . '/read-env.php';
+
+try {
+    $pdo = new PDO("mysql:host={$_ENV['DB_HOST']}", $_ENV['DB_USER'], $_ENV['DB_PASS']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo->exec("CREATE DATABASE IF NOT EXISTS {$_ENV['DB_NAME']};");
+
+    $pdo = new PDO(
+        "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']}",
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS'],
+        [PDO::MYSQL_ATTR_MULTI_STATEMENTS => true]
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $db_init_queries = file_get_contents(__DIR__ . '/db-init.sql');
+    $pdo->exec($db_init_queries);
+}
+catch (PDOException $e) {
+    error_log($e->getMessage());
+    exit;
+}
