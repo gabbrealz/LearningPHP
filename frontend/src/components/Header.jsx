f0079ef721881c1d34e47c7c5b08c322c2dbb239
unknown <christianagot11@gmail.com>
2026-01-19 09:08:23 +0800

    made logout button its own component for code cleanliness

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,38 @@
 import { Link, useNavigate } from "react-router-dom"
 
 export default function Header({ addToNotifs, setModalData, authenticatedUser, setAuthenticatedUser }) {
+  return (
+    <header className="z-100 fixed top-0 left-0 w-full py-4 bg-black px-4 sm:px-12 md:px-20 lg:px-28 xl:px-36">
+      <nav className="w-full flex">
+        <Link to="/" className="text-xl text-white px-2 py-1 flex items-center rounded-lg hover:text-slate-400 transition-[color]">
+          ExpenseTracker
+        </Link>
+        <div className="ml-auto w-1/2 flex justify-end items-center gap-2 lg:gap-4">
+          { authenticatedUser ?
+            <LogoutButton addToNotifs={addToNotifs} setModalData={setModalData} setAuthenticatedUser={setAuthenticatedUser} />
+            :
+            <>
+              <Link to="/login" className="
+                text-sm text-white px-2 py-1 border rounded-lg border-green-700 bg-green-700 transition-colors
+                lg:px-4 lg:border-white lg:bg-transparent lg:hover:border-green-700 lg:hover:bg-green-700
+              ">
+                Login
+              </Link>
+              <Link to="/sign-up" className="
+                text-sm text-white px-2 py-1 border rounded-lg border-indigo-600 bg-indigo-600 transition-colors
+                lg:px-4 lg:border-white lg:bg-transparent lg:hover:border-indigo-600 lg:hover:bg-indigo-600
+              ">
+                Sign Up
+              </Link>
+            </>
+          }
+        </div>
+      </nav>
+    </header>
+  )
+};
+
+function LogoutButton({ addToNotifs, setModalData, setAuthenticatedUser }) {
   const navigate = useNavigate();
 
   const handleLogout = async () => {
@@ -46,42 +78,17 @@ export default function Header({ addToNotifs, setModalData, authenticatedUser, s
       buttons: [
         { content: "Yes", onClick: handleLogout },
         { content: "No", onClick: () => setModalData({ show: false }) }
-      ]
+      ],
+      className: "w-75"
     });
   };
 
   return (
-    <header className="z-100 fixed top-0 left-0 w-full py-4 bg-black px-4 sm:px-12 md:px-20 lg:px-28 xl:px-36">
-      <nav className="w-full flex">
-        <Link to="/" className="text-xl text-white px-2 py-1 flex items-center rounded-lg hover:text-slate-400 transition-[color]">
-          ExpenseTracker
-        </Link>
-        <div className="ml-auto w-1/2 flex justify-end items-center gap-2 lg:gap-4">
-          { authenticatedUser ?
-            <button onClick={clickLogoutButton} className="
-              text-sm text-white px-2 py-1 border rounded-lg border-red-700 bg-red-700 transition-colors cursor-pointer
-              lg:px-4 lg:border-white lg:bg-transparent lg:hover:border-red-700 lg:hover:bg-red-700
-            ">
-              Logout
-            </button>
-            :
-            <>
-              <Link to="/login" className="
-                text-sm text-white px-2 py-1 border rounded-lg border-green-700 bg-green-700 transition-colors
-                lg:px-4 lg:border-white lg:bg-transparent lg:hover:border-green-700 lg:hover:bg-green-700
-              ">
-                Login
-              </Link>
-              <Link to="/sign-up" className="
-                text-sm text-white px-2 py-1 border rounded-lg border-indigo-600 bg-indigo-600 transition-colors
-                lg:px-4 lg:border-white lg:bg-transparent lg:hover:border-indigo-600 lg:hover:bg-indigo-600
-              ">
-                Sign Up
-              </Link>
-            </>
-          }
-        </div>
-      </nav>
-    </header>
-  )
-};
+    <button onClick={clickLogoutButton} className="
+      text-sm text-white px-2 py-1 border rounded-lg border-red-700 bg-red-700 transition-colors cursor-pointer
+      lg:px-4 lg:border-white lg:bg-transparent lg:hover:border-red-700 lg:hover:bg-red-700
+    ">
+      Logout
+    </button>
+  );
+}
